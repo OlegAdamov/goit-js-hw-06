b46a899f9a9531a7ba40dd9d46ed41f8b20cdade
Oleg Adamov <olegadamov84@gmail.com>
2022-08-07 19:13:24 +0300

    task - 04 Completed

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,37 +1,20 @@
-// Создай переменную counterValue в которой будет храниться текущее значение счетчика 
-//   и инициализируй её значением 0.
-// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай 
-//   значение счетчика.
-// Обновляй интерфейс новым значением переменной counterValue.
-
-
 
 const decrementBtn = document.querySelector('#counter button[data-action="decrement"]');
 const valueSpan = document.querySelector('#value');
 const incrementBtn = document.querySelector('#counter button[data-action="increment"]');
 
-
-console.log(decrementBtn)
-console.log(valueSpan)
-console.log(incrementBtn)
-
-const counterValue = 0;
-
+let counterValue = 0;
 
 decrementBtn.addEventListener('click', onBtnClickMinus) 
 
 function onBtnClickMinus() {
-    console.log('-1')
+    valueSpan.textContent = counterValue -= 1;
 }
 
 
-incrementBtn.addEventListener('click', onBtnClickPlus);
-
-function onBtnClickPlus() {
-    console.log('+1')
-
-
-}
+incrementBtn.addEventListener('click', () => {
+    valueSpan.textContent = counterValue += 1;
+});
 
 
 
