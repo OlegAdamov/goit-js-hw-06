2a71f2fc2369b700f88e0783ad7e284dbf995181
Oleg Adamov <olegadamov84@gmail.com>
2022-08-07 19:35:03 +0300

    task - 05 Completed

---
+++ src/js/task-05.js
@@ -0,0 +1,6 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+textInput.value === '' ? output.textContent = 'Anonymous' : output.textContent = event.currentTarget.value;
+});
