178c735784ae419e6288d7b8f4e9c65407034f2d
Oleg Adamov <olegadamov84@gmail.com>
2022-08-08 13:20:00 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
 
+const form = document.querySelector(".login-form");
+
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+      email.value === "" || password.value === "" ?
+    alert(`Все поля должны быть заполнены`) :
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+    event.currentTarget.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
