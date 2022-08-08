6334fbf65363d598e78d9b0742286416fccb0e0a
Oleg Adamov <olegadamov84@gmail.com>
2022-08-08 12:39:51 +0300

    Update task-07.js

---
+++ src/js/task-07.js
@@ -0,0 +1,8 @@
+const onInputId = document.querySelector('#font-size-control');
+const onSpanId = document.querySelector('#text');
+
+onInputId.addEventListener('input', spanIdText);
+
+function spanIdText(event) {
+    onSpanId.style.fontSize = `${+event.currentTarget.value}px`;
+}
