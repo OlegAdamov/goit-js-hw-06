fb70d4320276217edc5ee61658178f288b0e7329
Oleg Adamov <olegadamov84@gmail.com>
2022-08-08 12:14:09 +0300

    task - 06 Completed

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,35 +1,13 @@
 
-// Напиши скрипт, который при потере фокуса на инпуте (событие blur),
-//      проверяет его содержимое на правильное количество введённых символов.
-
-
-// Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
-// Если введено подходящее количество символов, то border инпута становится зелёным,
-//      если неправильное - красным.
-// Для добавления стилей, используй CSS-классы valid и invalid,
-//      которые мы уже добавили в исходные файлы задания.
-
-
 const onStyleInput = document.querySelector('#validation-input')
-console.log('onStyleInput', onStyleInput)
-// const onStyleInputValid = document.querySelector('#validation-input.valid')
-// console.log('onStyleInputValid', onStyleInputValid)
-// const onStyleInputInvalid = document.querySelector('#validation-input.invalid')
-// console.log('onStyleInputInvalid', onStyleInputInvalid)
-
-// const onPlaceholderInput = document.querySelector('input[data-length="6"]');
-// console.log('onPlaceholderInput', onPlaceholderInput)
 
 onStyleInput.addEventListener('blur', onInputBlur);
 
 function onInputBlur() {
-    // onStyleInput.classList.add(invalid)
+    onStyleInput.classList.add('invalid')
     if (+onStyleInput.value.length === +onStyleInput.dataset.length) {
-        onStyleInput.classList.remove('invalid')
-        onStyleInput.classList.add('valid')
-    } else {
-        onStyleInput.classList.remove('valid')
-        onStyleInput.classList.add('invalid')
+        onStyleInput.classList.toggle('invalid')
+        onStyleInput.classList.toggle('valid')
     };
 };
 
